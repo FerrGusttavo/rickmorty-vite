66cc1befee43a1a08096e2ed23418acb0552359d
FerrGusttavo <gustavo.augusto.f@outlook.com>
2025-07-30 21:52:51 -0300

    feat: implement episodes page with loading, pagination, and episode card components

---
+++ src/services/get-all-episodes.ts
@@ -0,0 +1,25 @@
+import { isAxiosError } from 'axios'
+import { api } from '@/lib/axios/api'
+import type {
+  GetAllEpisodesParams,
+  GetAllEpisodesResponse,
+} from '@/types/episode'
+
+export async function getAllEpisodes({
+  page,
+}: GetAllEpisodesParams): Promise<GetAllEpisodesResponse> {
+  try {
+    const response = await api.get<GetAllEpisodesResponse>(
+      `/episode?page=${page}`,
+    )
+    return response.data
+  } catch (err) {
+    if (isAxiosError(err)) {
+      if (err.response?.status === 404) {
+        throw err
+      }
+    }
+
+    throw err
+  }
+}
---
+++ src/types/episode.ts
@@ -0,0 +1,20 @@
+export interface EpisodeListItem {
+  id: number
+  name: string
+  air_date: string
+  episode: string
+}
+
+interface Info {
+  count: number
+  pages: number
+}
+
+export interface GetAllEpisodesParams {
+  page: number
+}
+
+export interface GetAllEpisodesResponse {
+  info: Info
+  results: EpisodeListItem[]
+}
